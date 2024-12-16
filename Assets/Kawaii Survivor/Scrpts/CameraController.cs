a614af66a9055bb9a3a1e9871159984cdcd5cae5
AliFar26 <master.ali0101@gmail.com>
2024-12-16 18:13:59 +0330

    Added cameraFollow(Start of Arena implementaion)

---
+++ Assets/Kawaii Survivor/Scrpts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    [Header("Element")]
+    [SerializeField] private Transform target;
+
+    [Header("Setting")]
+    [SerializeField] private Vector2 minManxXY;
+    private void LateUpdate()
+    {
+        if (target != null)
+            return;
+
+        Vector3 targetPosition = target.position;
+        targetPosition.z = -10;
+
+        targetPosition.x = Mathf.Clamp(targetPosition .x ,- minManxXY.x, minManxXY.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, -minManxXY.y, minManxXY.y);
+
+
+
+        transform.position = targetPosition;
+    }
+}
--- Assets/Kawaii Survivor/Scrpts/PlayerController.cs
+++ Assets/Kawaii Survivor/Scrpts/PlayerController.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Element")]
+    [SerializeField]private MobileJoystick mobileJoystick;
+
+    [Header("Setting")]
+    [SerializeField] private float moveSpeed;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.right;
+        //rb.velocity = Vector2.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = mobileJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
     }
 }
