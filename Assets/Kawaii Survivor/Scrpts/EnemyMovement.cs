631450075984346506a834568a1092a657e5d5ac
AliFar26 <master.ali0101@gmail.com>
2024-12-17 19:12:34 +0330

    Enemy Spawn Sequence

--- Assets/Kawaii Survivor/Scrpts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scrpts/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,22 @@ public class EnemyMovement : MonoBehaviour
     [Header("Element")]
     private Player player;
 
+    [Header("Spawn Sequence Related")]
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private SpriteRenderer spawnIndicator;
+    private bool hasSpawned ;
+
     [Header("Setting")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float playerDetectionRadius;
+
+    [Header("Debug")]
+    [SerializeField] private bool gizmos;
+
+    [Header("Debug")]
+    [SerializeField] private ParticleSystem passAwayParticles;
+
+
 
     void Start()
     {
@@ -17,18 +32,89 @@ public class EnemyMovement : MonoBehaviour
         if (player == null)
         {
             Destroy(gameObject);
-
-
         }
+
+        //Hide the renderer
+        renderer.enabled = false;
+        
+        //Show the spawn indicator
+        spawnIndicator.enabled = true;
+
+
+        //Scale up & down the spawn indicator
+        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
+        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f)
+            .setLoopPingPong(4)
+            .setOnComplete(SpawnSequenceCompleted);
+
+
+
+        //Show the enemy after 3 seconds
+        //Hide the indicator
+
+        //Prevent Followint & Attacking functionn during the spawn sequence
+
+
+    }
+
+    private void SpawnSequenceCompleted()
+    {
+        //Show the enemy after 3 seconds
+        //Hide the indicator
+
+        //Hide the renderer
+        renderer.enabled = true;
+
+        //Show the spawn indicator
+        spawnIndicator.enabled = false;
+
+        hasSpawned = true;
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasSpawned)
+            return;
+
+        FollowPlayer();
+        TryAttack();
+    }
+
+
+    private void FollowPlayer()
     {
         Vector2 direction = (player.transform.position - transform.position).normalized;
 
         Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
 
         transform.position = targetPosition;
+    }
+
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer <= playerDetectionRadius)
+        {
+            PassAway();
+        }
+
+    }
+
+    private void PassAway()
+    {
+        //Unparent the particle & play them
+        passAwayParticles.transform.SetParent(null);
+        passAwayParticles.Play();
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!gizmos)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);   
     }
 }
