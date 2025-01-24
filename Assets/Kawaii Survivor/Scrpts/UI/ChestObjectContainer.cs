cf4dc6e2d2dc6f8ac9e10ae94629518249e03a9d
AliFar26 <master.ali0101@gmail.com>
2025-01-24 05:07:03 +0330

    chapter 25 - end of video 154

--- Assets/Kawaii Survivor/Scrpts/UI/ChestObjectContainer.cs
+++ Assets/Kawaii Survivor/Scrpts/UI/ChestObjectContainer.cs
@@ -20,6 +20,7 @@ public class ChestObjectContainer : MonoBehaviour
 
     [field: SerializeField] public Button TakeButton { get; private set; }
     [field: SerializeField] public Button RecycleButton { get; private set; }
+    [SerializeField] public TextMeshProUGUI recyclePriceText;
 
     [Header(" Color ")]
     [SerializeField] private Image[] levelDependentImage;
@@ -28,6 +29,7 @@ public class ChestObjectContainer : MonoBehaviour
     {
         icon.sprite = objectData.Icon;
         nameText.text = objectData.Name ;
+        recyclePriceText.text = objectData.RecyclePrice.ToString() ;
 
         Color imageColor = ColorHolder.GetColor(objectData.Rarity);
         nameText.color = imageColor;
