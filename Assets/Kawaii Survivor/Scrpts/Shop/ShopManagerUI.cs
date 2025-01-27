fabb4d73e2a43328c6bdc6d21a05ca3f7efc0d07
AliFar26 <master.ali0101@gmail.com>
2025-01-27 09:00:07 +0330

    chapter 27 - end of video 171

--- Assets/Kawaii Survivor/Scrpts/Shop/ShopManagerUI.cs
+++ Assets/Kawaii Survivor/Scrpts/Shop/ShopManagerUI.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManagerUI : MonoBehaviour
 {
 
     [Header("Player Stat Element")]
     [SerializeField] private RectTransform playerStatsPanel;
-    [SerializeField] private GameObject playerStatsClosePanel;
+    [SerializeField] private RectTransform playerStatsClosePanel;
+    private Vector2 playrStatsOpenedPos;
+    private Vector2 playrStatsClosedPos;
 
     // Start is called before the first frame update
     IEnumerator Start()
@@ -21,8 +24,14 @@ public class ShopManagerUI : MonoBehaviour
     private void ConfigurePlayerStatsPanel()
     {
         float width = Screen.width / (4 * playerStatsPanel.lossyScale.x);
-        playerStatsPanel.offsetMax = playerStatsPanel.offsetMax.With(x : width);
+        playerStatsPanel.offsetMax = playerStatsPanel.offsetMax.With(x: width);
 
+        playrStatsOpenedPos = playerStatsPanel.anchoredPosition;
+        playrStatsClosedPos = playrStatsOpenedPos - Vector2.right * width;
+
+        playerStatsPanel.anchoredPosition = playrStatsClosedPos;
+
+        HidePlayerStats();
     }
 
     // Update is called once per frame
@@ -35,14 +44,32 @@ public class ShopManagerUI : MonoBehaviour
     public void ShowPlayerStats()
     {
         playerStatsPanel.gameObject.SetActive(true);
-        playerStatsClosePanel.SetActive(true);
+        playerStatsClosePanel.gameObject.SetActive(true);
+        playerStatsClosePanel.GetComponent<Image>().raycastTarget = true;
+
+        LeanTween.cancel(playerStatsPanel);
+        LeanTween.move(playerStatsPanel, playrStatsOpenedPos , .5f).setEase(LeanTweenType.easeInCubic);
+
+        LeanTween.cancel(playerStatsClosePanel);
+        LeanTween.alpha(playerStatsClosePanel, 0.8f , 0.5f).setRecursive(false);
     }
 
     [NaughtyAttributes.Button]
     public void HidePlayerStats()
     {
-        playerStatsPanel.gameObject.SetActive(false);
-        playerStatsClosePanel.SetActive(false);
+        //playerStatsPanel.gameObject.SetActive(false);
+        //playerStatsClosePanel.gameObject.SetActive(false);
+
+        playerStatsClosePanel.GetComponent<Image>().raycastTarget = false;
+
+        LeanTween.cancel(playerStatsPanel);
+        LeanTween.move(playerStatsPanel, playrStatsClosedPos, .5f)
+            .setEase(LeanTweenType.easeOutCubic)
+            .setOnComplete(() => playerStatsPanel.gameObject.SetActive(false));
+
+        LeanTween.cancel(playerStatsClosePanel);
+        LeanTween.alpha(playerStatsClosePanel, 0, 0.5f).setRecursive(false)
+            .setOnComplete(() => playerStatsClosePanel.gameObject.SetActive(false));
 
     }
 }
